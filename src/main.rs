ef0f564946fb2b954e116436700823cfef2ec576
PurpleCraft999 <152620081+PurpleCraft999@users.noreply.github.com>
2026-01-14 21:19:19 -0500

    i need to be better at git commits

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use std::str::FromStr;
+
+use clap::Parser;
+
+// use serde::Deserialize;
+
+mod pokedex;
+mod pokemon;
+use pokedex::PokeDex;
+use pokemon::{PokedexColor, PokemonType};
+use strum::VariantArray;
+
+use crate::{
+    pokedex::{MAX_POKEDEX_NUM, PokedexSearchResualt},
+    pokemon::{POKEMON_NAME_ARRAY, compute_similarity},
+};
+fn main() {
+    let args = Args::parse();
+
+    let pokedex = match PokeDex::new() {
+        Ok(dex) => dex,
+        Err(e) => panic!("could not run: {e}"),
+    };
+    // let pkmn1 = pokedex.find_by_natinal_dex_number(5).unwrap();
+    // let pkmn2 = pokedex.find_by_natinal_dex_number(7).unwrap();
+    let pokemon: PokedexSearchResualt = match args.search_value {
+        SearchValue::Dex { dex_num } => pokedex.find_by_natinal_dex_number(dex_num).into(),
+        SearchValue::Name { name } => pokedex.find_by_name(name).into(),
+        SearchValue::Type { ptype } => pokedex.find_by_type(ptype).into(),
+        SearchValue::Color { color } => pokedex.find_by_color(color).into(),
+    };
+    pokemon.print_data(args.detailed);
+}
+
+///pokedex
+#[derive(clap::Parser)]
+#[command(version)]
+struct Args {
+    ///takes a pokemon's name,color,type or dex number
+    #[arg(value_parser = SearchValue::parser)]
+    search_value: SearchValue,
+    ///prints a more detailed version of the data
+    #[arg(long)]
+    detailed: bool,
+}
+
+#[derive(clap::Subcommand, Clone)]
+///test
+enum SearchValue {
+    
+    Dex { dex_num: u16 },
+    
+    Name { name: String },
+    Type {
+        #[arg(value_enum)]
+        ptype: PokemonType,
+    },
+    Color {
+        #[arg(value_enum)]
+        color: PokedexColor,
+    },
+}
+impl SearchValue {
+    fn parser(input: &str) -> Result<Self, String> {
+        if let Ok(dex_num) = input.parse::<u16>() {
+            if 1 < dex_num && dex_num <= MAX_POKEDEX_NUM {
+                return Ok(SearchValue::Dex { dex_num });
+            } else {
+                return Err(format!(
+                    "the search value must be between 1-{MAX_POKEDEX_NUM}"
+                ));
+            }
+        } else if let Ok(ptype) = PokemonType::from_str(input) {
+            // println!("is type");
+            return Ok(Self::Type { ptype });
+        } else if let Ok(color) = PokedexColor::from_str(input) {
+            return Ok(SearchValue::Color { color });
+        }
+        let mut potintal_name = compute_similarity(input, &POKEMON_NAME_ARRAY);
+
+        if potintal_name.len() == 1 {
+            let first_name = &potintal_name[0];
+            if first_name == input {
+                return Ok(SearchValue::Name {
+                    name: first_name.clone(),
+                });
+            }
+        }
+        if !potintal_name.is_empty() {
+            let mut err_vec = Vec::new();
+            err_vec.append(&mut potintal_name);
+            err_vec.append(&mut compute_similarity(input, PokedexColor::VARIANTS));
+            err_vec.append(&mut compute_similarity(input, PokemonType::VARIANTS));
+            let mut did_you_mean_str = String::with_capacity(err_vec.len());
+            did_you_mean_str.push_str("did you mean:");
+            for string in err_vec {
+                did_you_mean_str.push_str(&string);
+                did_you_mean_str.push(',');
+            }
+            did_you_mean_str.pop();
+             Err(did_you_mean_str)
+        } else {
+             Err("sorry we couldnt find anything".into())
+        }
+    }
+}
---
+++ src/pokedex.rs
@@ -0,0 +1,117 @@
+use crate::pokemon::{PokedexColor, Pokemon, PokemonType};
+use memmap2::Mmap;
+use rayon::iter::{ParallelBridge, ParallelIterator};
+use std::{
+    // fs::File,
+    io::BufRead,
+};
+
+pub type SingleSearchReturn = Option<Pokemon>;
+pub type MultiSearchReturn = Vec<Pokemon>;
+pub enum PokedexSearchResualt {
+    Single(SingleSearchReturn),
+    Multi(MultiSearchReturn),
+}
+impl PokedexSearchResualt {
+    pub fn print_data(&self, is_detailed: bool) {
+        match self {
+            PokedexSearchResualt::Single(single) => match single {
+                Some(pkmn) => pkmn.print(is_detailed),
+                None => println!("sorry we couldnt find any thing"),
+            },
+            PokedexSearchResualt::Multi(vec) => {
+                for pkmn in vec {
+                    pkmn.print(is_detailed)
+                }
+                if vec.is_empty() {
+                    println!("sorry we couldnt find any thing")
+                }
+            }
+        }
+    }
+    // pub fn get_similar_words(&self){
+    //     ma
+    // }
+}
+impl From<SingleSearchReturn> for PokedexSearchResualt {
+    fn from(value: SingleSearchReturn) -> Self {
+        Self::Single(value)
+    }
+}
+impl From<MultiSearchReturn> for PokedexSearchResualt {
+    fn from(mut value: MultiSearchReturn) -> Self {
+        value.sort_by(|o, t| {
+            if o.get_dex_number() > t.get_dex_number() {
+                std::cmp::Ordering::Greater
+            } else {
+                std::cmp::Ordering::Less
+            }
+        });
+        Self::Multi(value)
+    }
+}
+
+pub struct PokeDex {
+    mmap: Mmap,
+}
+impl PokeDex {
+    pub fn new() -> Result<Self, std::io::Error> {
+        // File::(include_str!("../pokedex.jsonl"));
+        // let file = File::open("pokedex.jsonl").unwrap();
+        let data = include_bytes!("../pokedex.jsonl");
+        let mut mmap = memmap2::MmapOptions::new().len(data.len()).map_anon()?;
+        mmap.copy_from_slice(data);
+        let mmap = mmap.make_read_only()?;
+        Ok(Self { mmap })
+    }
+    #[allow(clippy::type_complexity)]
+    fn mmap_to_pokemap(
+        &self,
+    ) -> rayon::iter::Map<
+        rayon::iter::IterBridge<
+            std::iter::MapWhile<
+                std::io::Lines<&[u8]>,
+                impl FnMut(Result<String, std::io::Error>) -> Option<String>,
+            >,
+        >,
+        impl Fn(String) -> Pokemon,
+    > {
+        self.mmap
+            .lines()
+            .map_while(|item| item.ok())
+            .par_bridge()
+            .map(|line| serde_json::from_str::<Pokemon>(&line).unwrap())
+    }
+
+    fn find_one_pokemon<P: Fn(&Pokemon) -> bool + Sync + Send>(
+        &self,
+        find: P,
+    ) -> SingleSearchReturn {
+        self.mmap_to_pokemap().find_first(find)
+    }
+
+    fn find_many_pokemon<P: Fn(&Pokemon) -> bool + Sync + Send>(
+        &self,
+        filter: P,
+    ) -> MultiSearchReturn {
+        self.mmap_to_pokemap().filter(filter).collect()
+    }
+    pub fn find_by_type(&self, ptype: PokemonType) -> MultiSearchReturn {
+        // println!("find_by_type");
+        self.find_many_pokemon(|pokemon| {
+            pokemon.get_primary_type() == ptype || pokemon.get_seconary_type() == ptype
+        })
+    }
+
+    pub fn find_by_natinal_dex_number(&self, dex_num: u16) -> SingleSearchReturn {
+        self.find_one_pokemon(|pokemon| pokemon.get_dex_number() == dex_num)
+    }
+    pub fn find_by_name(&self, name: String) -> SingleSearchReturn {
+        self.find_one_pokemon(|pkmn| pkmn.get_name() == &name)
+    }
+    pub fn find_by_color(&self, color: PokedexColor) -> MultiSearchReturn {
+        self.find_many_pokemon(|pkmn| pkmn.get_color() == color)
+    }
+}
+
+pub const MAX_POKEDEX_NUM: u16 = 1025;
